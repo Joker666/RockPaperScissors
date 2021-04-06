557f9d4837324bbb7c7f68514962f2504a388536
Joker666 <mr.k779@gmail.com>
2021-04-06 21:47:40 +0600

    Removed unused messages

--- src/PaperScissorsStone/Client.java
+++ src/PaperScissorsStone/Client.java
@@ -1,104 +1,43 @@
-/**
- * The client class creates a connection to the server at default port 1337.
- * Waits for the user to input a single character with the keyboard. The
- * character has to be ’R’ (rock), ’P’ (paper) or ’S’ (scissors). Sends the
- * character to the server via the TCP protocol. Waits for a reply from the
- * Server. Prints the message received from the Server. Closes the connection.
- *
- * @author Mathias Schilling <https://github.com/pinkbigmacmedia>
- * @version 1.0
- * 
- */
-
 package PaperScissorsStone;
 
 import java.io.*;
 import java.net.*;
 
 class Client {
+    private static final String host = "localhost";
+    private static final Integer port = 9090;
 
-    /**
-     * The host
-     * 
-     * @var string
-     * 
-     */
-    private static String host = "localhost";
+    private static final String msgWelcome = "--- Get Started\n";
 
-    /**
-     * The port
-     * 
-     * @var integer
-     */
-    private static Integer port = 1337;
+    public static void main(String[] args) throws Exception {
 
-    /**
-     * The version of the client class
-     * 
-     * @var double
-     */
-    private static Double versionNumber = 1.0;
+		String input;
+		String response;
 
-    /**
-     * A short welcome msg
-     * 
-     * @var string
-     */
-    private static String msgWelcome = "--- Welcome to Paper Scissors Stone V. "
-	    + versionNumber + " --- \n";
+		System.out.println(Client.msgWelcome);
 
-    /**
-     * The help context
-     * 
-     * @var string
-     * 
-     */
-    private static String msgRules = "\nRule set:\n - (R)ock beats (S)cissors\n - (S)cissors beats (P)aper\n - (P)aper beats (R)ock\n";
+		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+		Socket clientSocket = new Socket(Client.host, Client.port);
+		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-    public static void main(String args[]) throws Exception {
+		do {
+			// Prompt user for select rock, paper or scissors ...
+			System.out.println("Select (1)Rock (2)Paper, (3)Scissors: ");
+			input = inFromUser.readLine();
+		} while (!input.equals("1") && !input.equals("2") && !input.equals("3"));
 
-	String input = "";
-	String response;
+		// Transmit input to the server and provide some feedback for the user
+		outToServer.writeBytes(input + "\n");
+		System.out.println("\nYou have entered (" + input + ")");
 
-	System.out.println(Client.msgWelcome);
+		// Catch responses
+		response = inFromServer.readLine();
 
-	BufferedReader inFromUser = new BufferedReader(new InputStreamReader(
-		System.in));
-	Socket clientSocket = new Socket(Client.host, Client.port);
-	DataOutputStream outToServer = new DataOutputStream(
-		clientSocket.getOutputStream());
-	BufferedReader inFromServer = new BufferedReader(new InputStreamReader(
-		clientSocket.getInputStream()));
+		// Display responses
+		System.out.println("Response from server: " + response);
 
-	do {
-
-	    if (input.equals("-rules")) {
-		System.out.println(Client.msgRules);
-	    }
-
-	    // Prompt user for select rock, paper or scissors ...
-	    System.out
-		    .println("Start the game by selecting (R)ock (P)aper, (S)cissors");
-	    System.out.print("or type \"-rules\" in order to see the rules: ");
-	    input = inFromUser.readLine();
-
-	} while (!input.equals("R") && !input.equals("P") && !input.equals("S"));
-
-	// Transmit input to the server and provide some feedback for the user
-	outToServer.writeBytes(input + "\n");
-	System.out
-		.println("\nYour input ("
-			+ input
-			+ ") was successfully transmitted to the server. Now just be patient and wait for the result ...");
-
-	// Catch respones
-	response = inFromServer.readLine();
-
-	// Display respones
-	System.out.println("Response from server: " + response);
-
-	// Close socket
-	clientSocket.close();
-
+		// Close socket
+		clientSocket.close();
     }
 }
--- src/PaperScissorsStone/Server.java
+++ src/PaperScissorsStone/Server.java
@@ -1,109 +1,28 @@
-/**
- * The server class waits for the connection of the two clients client_1
- * and client_2 (in either order) on port 1337. Receives one character
- * from client_1 and one character from client_2 (in either order) and
- * calculates the winner of the game based on a rule set. After sending a
- * correspondent massage to each client the server waits again for two
- * clients to connect.
- *
- * @author Mathias Schilling <https://github.com/pinkbigmacmedia>
- * @version 1.0
- * 
- */
-
 package PaperScissorsStone;
 
-import java.io.*;
-import java.net.*;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
+	private static final Integer port = 9090;
 
-	/**
-	 * The defautl port number
-	 * 
-	 * @var integer
-	 * 
-	 */
-	private static Integer port = 1337;
-
-	/**
-	 * The script version number
-	 * 
-	 * @var integer
-	 * 
-	 */
-	private static Double versionNumber = 1.0;
-
-	/**
-	 * The welcome boiler plate
-	 * 
-	 * @var string
-	 * 
-	 */
-	private static String welcomeMsg = "--- Welcome to Paper Scissors Stone Server V. " + versionNumber + " --- \n";
-
-	/**
-	 * Function takes an integer x as an input and returns the boolean value
-	 * true if the input is strictly greater than 0 and less than or equal to
-	 * 65535.
-	 * 
-	 * @param integer
-	 *            x
-	 * @return boolean
-	 */
-	private static boolean validPort(Integer x) {
-		return x >= 1 && x <= 65535 ? true : false;
-	}
-
-	/**
-	 * Function prompts the user to choose a specific port number or to press
-	 * enter in order to continue with default setting (Server.port).
-	 * 
-	 * The returned integer strictly greater than 0 and less than or equal to
-	 * 65535.
-	 * 
-	 * @return integer
-	 */
-	private static int getPort() {
-
-		Integer input;
-
-		Scanner sc = new Scanner(System.in);
-
-		do {
-			System.out.print("Please select a port by entering an integer value between 1 and 65535 or\n");
-			System.out.print("insert \"0\" in order to continue with the default setting (" + Server.port + "): ");
-			input = sc.nextInt();
-
-		} while (input != 0 && !Server.validPort(input));
-
-		sc.close();
-
-		return input == 0 ? Server.port : input;
-	}
-
-	public static void main(String args[]) throws Exception {
-
+	public static void main(String[] args) throws Exception {
 		String resClient_1 = "";
 		String resClient_2 = "";
 		String inputClient_1;
 		String inputClient_2;
 
-		// Print welcome msg
-		System.out.println(Server.welcomeMsg);
-
-		// Set port
-		Server.port = Server.getPort();
-
 		// Create new server socket & dump out a status msg
-		ServerSocket welcomeSocket = new ServerSocket(Server.port);
-		System.out.println("\nOk, we're up and running on port " + welcomeSocket.getLocalPort() + " ...");
+		ServerSocket serverSocket = new ServerSocket(Server.port);
+		System.out.println("Server is running on port " + serverSocket.getLocalPort() + "\n");
 
-		while (!welcomeSocket.isClosed()) {
+		while (!serverSocket.isClosed()) {
 
 			// Player one
-			Socket client_1 = welcomeSocket.accept();
+			Socket client_1 = serverSocket.accept();
 			if (client_1.isConnected()) {
 				System.out.println("\nPlayer one (" + (client_1.getLocalAddress().toString()).substring(1) + ":"
 						+ client_1.getLocalPort() + ") has joined ... waiting for player two ...");
@@ -112,7 +31,7 @@ public class Server {
 			BufferedReader inClient_1 = new BufferedReader(new InputStreamReader(client_1.getInputStream()));
 
 			// Player two
-			Socket client_2 = welcomeSocket.accept();
+			Socket client_2 = serverSocket.accept();
 			if (client_2.isConnected()) {
 				System.out.println("Player two (" + (client_2.getLocalAddress().toString()).substring(1) + ":"
 						+ client_1.getLocalPort() + ") has joined ... lets start ...");
@@ -124,66 +43,31 @@ public class Server {
 			inputClient_1 = inClient_1.readLine();
 			inputClient_2 = inClient_2.readLine();
 
-			/**
-			 * If the characters received from C1 and C2 are the same then the
-			 * server sends back to both clients the string "DRAW".
-			 */
 			if (inputClient_1.equals(inputClient_2)) {
 				resClient_1 = "Draw";
 				resClient_2 = "Draw";
-				System.out.println("It's a draw.");
-			}
-			/**
-			 * If the server receives ’R’ from C1 and ’S’ from C2 it sends the
-			 * string "YOU WIN" to C1 and the string "YOU LOSE" to C2.
-			 */
-			else if (inputClient_1.equals("R") && inputClient_2.equals("S")) {
+				System.out.println("It'3 a draw.");
+			} else if (inputClient_1.equals("1") && inputClient_2.equals("3")) {
 				resClient_1 = "You win";
 				resClient_2 = "You lose";
 				System.out.println("Player one wins.");
-
-			}
-			/**
-			 * If the server receives ’S’ from C1 and ’R’ from C2 it sends the
-			 * string "YOU LOSE" to C1 and the string "YOU WIN" to C2.
-			 */
-			else if (inputClient_1.equals("S") && inputClient_2.equals("R")) {
+			} else if (inputClient_1.equals("3") && inputClient_2.equals("1")) {
 				resClient_1 = "You lose";
 				resClient_2 = "You win";
 				System.out.println("Player two wins.");
-			}
-			/**
-			 * If the server receives ’R’ from C1 and ’P’ from C2 it sends the
-			 * string "YOU LOSE" to C1 and the string "YOU WIN" to C2.
-			 */
-			else if (inputClient_1.equals("R") && inputClient_2.equals("P")) {
+			} else if (inputClient_1.equals("1") && inputClient_2.equals("2")) {
 				resClient_1 = "You lose";
 				resClient_2 = "You win";
 				System.out.println("Player two wins.");
-			}
-			/**
-			 * If the server receives ’P’ from C1 and ’R’ from C2 it sends the
-			 * string "YOU WIN" to C1 and the string "YOU LOSE" to C2.
-			 */
-			else if (inputClient_1.equals("P") && inputClient_2.equals("R")) {
+			} else if (inputClient_1.equals("2") && inputClient_2.equals("1")) {
 				resClient_1 = "You win";
 				resClient_2 = "You lose";
 				System.out.println("Player one wins.");
-			}
-			/**
-			 * If the server receives ’S’ from C1 and ’P’ from C2 it sends the
-			 * string "YOU WIN" to C1 and the string "YOU LOSE" to C2.
-			 */
-			else if (inputClient_1.equals("S") && inputClient_2.equals("P")) {
+			} else if (inputClient_1.equals("3") && inputClient_2.equals("2")) {
 				resClient_1 = "You win";
 				resClient_2 = "You lose";
 				System.out.println("Player one wins.");
-			}
-			/**
-			 * If the server receives ’P’ from C1 and ’S’ from C2 it sends the
-			 * string "YOU LOSE" to C1 and the string "YOU WIN" to C2.
-			 */
-			else if (inputClient_1.equals("P") && inputClient_2.equals("S")) {
+			} else if (inputClient_1.equals("2") && inputClient_2.equals("3")) {
 				resClient_1 = "You lose";
 				resClient_2 = "You win";
 				System.out.println("Player two wins.");
@@ -196,7 +80,6 @@ public class Server {
 			client_2.close();
 
 			System.out.println("\nWaiting for new players ...\n");
-
 		}
 	}
 }
